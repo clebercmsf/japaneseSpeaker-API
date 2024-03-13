bf8131a854472caa0515eca5a880d4763f68ec24
clebercmsf <cleber.cmsf@gmail.com>
2024-03-13 15:11:11 -0300

    Inicial Commit

---
+++ src/server.ts
@@ -0,0 +1,50 @@
+import fastify from 'fastify';
+import { TextToSpeechClient } from '@google-cloud/text-to-speech';
+import { z } from 'zod';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = fastify();
+const client = new TextToSpeechClient();
+
+app.post("/textConverter", async (req, res) => {
+  const converterTextSchema = z.object({
+    text: z.string()
+  });
+
+  const { text } = converterTextSchema.parse(req.body);
+
+  const request = {
+    audioConfig: {
+      audioEncoding: "LINEAR16" as const,
+      effectsProfileId: [
+        "handset-class-device"
+      ],
+      pitch: 0,
+      speakingRate: 1
+    },
+    input: {
+      text: text
+    },
+    voice: {
+      languageCode: "ja-JP",
+      name: "ja-JP-Standard-B"
+    }
+  }
+
+  const [response] = await client.synthesizeSpeech(request);
+  const audio = response.audioContent;
+
+  res.header("Content-Type", "audio/mpeg");
+  res.send(audio);
+});
+
+
+app.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333
+}).then(() => {
+  console.log('HTTP Server Running');
+}).catch((err) => {
+  console.log(err.message);
+});
